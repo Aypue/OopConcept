e2beb237a6554973e89d3aff04b07f47ddef5ac1
Jasmine Sarsaba <jaja.srsb@gmail.com>
2026-03-04 22:37:07 +0800

    Story.java

--- GameText/Story.java
+++ GameText/Story.java
@@ -1,3 +1,27 @@
+abstract class Story {
+    protected String introduction;
+    protected String encounter;
+    protected String defeat;
+
+    public Story(String introduction, String encounter, String defeat) {
+        this.introduction = introduction;
+        this.encounter = encounter;
+        this.defeat = defeat;
+    }
+
+    public void introduction() {
+        System.out.println(introduction);
+    }
+
+    public void encounter() {
+        System.out.println(encounter);
+    }
+
+    public void defeat() {
+        System.out.println(defeat);
+    }
+}
+
 abstract class CharacterBackgroundStory extends Story {
     public CharacterBackgroundStory(String introduction, String encounter, String defeat) {
         super(introduction, encounter, defeat);
